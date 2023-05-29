2f9a576bbd0cf730e55a12ec34a891e3340b7176
Danijel Savkovic <danijel.savkovic@yahoo.com>
2023-05-29 10:15:47 +0200

    added assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [];
 
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class SkillsComponent {
 
+  skills = [
+    {
+      img: 'assets/img/iconSkills/javascript.png',
+      title: 'JavaScript',
+    },
+    {
+      img: 'assets/img/iconSkills/angular.png',
+      title: 'Angular',
+    },
+    {
+      img: 'assets/img/iconSkills/html.png', 
+      title: 'HTML',
+    },
+    {
+      img: 'assets/img/iconSkills/css.png',
+      title: 'CSS',
+    },
+    {
+      img: 'assets/img/iconSkills/scrum.png',
+      title: 'Scrum',
+    },
+    {
+      img: 'assets/img/iconSkills/git.png',
+      title: 'Git',
+    },
+    {
+      img: 'assets/img/iconSkills/material _design.png',
+      title: 'Material Design',
+    },
+    {
+      img: 'assets/img/iconSkills/rest-api.png',
+      title: 'Rest API',
+    },
+    {
+      img: 'assets/img/iconSkills/firebase.png',
+      title: 'Firebase',
+    },
+  ];
 }
