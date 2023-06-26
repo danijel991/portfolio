160bc4559a498d76b7110e924f52145b8e5801de
Danijel Savkovic <danijel.savkovic@yahoo.com>
2023-06-27 00:59:30 +0200

    updated scroll to top legal

--- src/app/legal/legal.component.ts
+++ src/app/legal/legal.component.ts
@@ -9,6 +9,10 @@ import { ScrollService } from '../scroll.service';
 export class LegalComponent {
  constructor(private scrollService: ScrollService, private elementRef: ElementRef, private renderer: Renderer2) {}
 
+ ngOnInit(): void {
+  this.scrollService.scrollToTop();
+}
+
   scrollToTop(): void {
     this.scrollService.scrollToTop();
   }
